341248e8d3463b178e0c00f7a286739ef5efe02b
alexandre gaillot <gaillotalexandre@yahoo.fr>
2022-03-11 15:06:04 +0100

    première entité => Category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -1,10 +1,19 @@
 <?php
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity()]
 class Category {
 
+    #[
+        ORM\Id,
+        ORM\GeneratedValue(),
+        ORM\Column(type:"integer")
+    ]
     private int $id;
 
+    #[ORM\Column(type:"string", length: "100")]
     private string $name;
 
     public function getId(): int
